6f9e021384c454bee8508b4b3d8f680769c34820
reinhold <reinhold6@schule.bremen.de>
2024-01-17 09:14:02 +0100

    Component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddQualificationComponent } from './add-qualification/add-qualification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [CommonModule, EmployeeListComponent, AddQualificationComponent]
 })
 export class AppComponent {
   title = 'lf10StarterNew';
