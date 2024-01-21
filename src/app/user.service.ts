1d70c52b82daad255ed1b7f4a42099509a6c44ce
Marius Meschter <git@onestay.moe>
2024-01-21 14:30:56 +0100

    add the UserService + injection of token

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './auth.config';
+import { filter } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authCodeFlowConfig);
+  }
+
+  getAccessToken() {
+    return this.oauthService.getAccessToken();
+  }
+
+  login() {
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+
+    this.oauthService.events
+      .pipe(filter((e) => e.type === 'token_received'))
+      .subscribe(() => {
+        this.oauthService.loadUserProfile();
+        console.log(this.oauthService.getAccessToken());
+      });
+  }
 }
