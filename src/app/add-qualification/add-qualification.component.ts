eae8d6e73cde43f10b20b5074a7f2187a5438ffc
reinhold <reinhold6@schule.bremen.de>
2024-01-31 11:00:16 +0100

    PopUp ready for addQualification

--- src/app/add-qualification/add-qualification.component.ts
+++ src/app/add-qualification/add-qualification.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QualificationService } from '../services/qualification.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-qualification',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-qualification.component.html',
   styleUrl: './add-qualification.component.css',
 })
-export class AddQualificationComponent {}
+export class AddQualificationComponent {
+  ;
+  qulificaionName:string ='';
+  constructor(private qualificationService: QualificationService,protected activeModal :NgbActiveModal){
+  }
+
+  addQualification(): void{
+    if(this.qulificaionName != '')
+      this.qualificationService.createNewQualification(this.qulificaionName).subscribe();
+  }
+
+}
