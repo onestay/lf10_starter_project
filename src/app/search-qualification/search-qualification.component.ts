707dc975bd49b45dfc03a7f8a0da5bea2b72475b
Leon Hefling <hefling@informationsgesellschaft.com>
2024-01-31 12:35:47 +0100

    search qualification works

--- src/app/search-qualification/search-qualification.component.ts
+++ src/app/search-qualification/search-qualification.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QualificationService } from '../services/qualification.service';
+import { FormsModule } from '@angular/forms';
+import { Qualification } from '../model/Qualification';
+import { Observable } from 'rxjs/internal/Observable';
+import { of } from 'rxjs/internal/observable/of';
+import { map } from 'rxjs/internal/operators/map';
+import Fuse from 'fuse.js';
 
 @Component({
   selector: 'app-search-qualification',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-qualification.component.html',
   styleUrl: './search-qualification.component.css',
 })
-export class SearchQualificationComponent {
-  searchFor;
+export class SearchQualificationComponent implements OnInit {
+  allQualifications: Observable<Qualification[]> = of([]);
+  qualifications: Observable<Qualification[]> = of([]);
+  searchFor: string = '';
+  constructor(private qualificationService: QualificationService) {}
+
+  ngOnInit(): void {
+    this.allQualifications = this.qualificationService.getAllQualifications();
+    this.onFilterType();
+  }
+
+  onFilterType() {
+    this.qualifications = this.allQualifications.pipe(
+      map((qualifications) => {
+        if (!this.searchFor) {
+          return qualifications;
+        }
+        const fuse = new Fuse(qualifications, {
+          keys: ['skill'],
+        });
+        return fuse.search(this.searchFor).map((result) => result.item);
+      }),
+    );
+  }
 }
